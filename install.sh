4792a65d44a8d326613807e04cd9a25314098c93
wiwyil2tr <108447154+wiwyil2tr@users.noreply.github.com>
2023-08-20 15:26:02 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@
 set -u
 set -e
 
+# define some colors for output
+RED=$(printf '\033[31m')
+GREEN=$(printf '\033[32m')
+YELLOW=$(printf '\033[33m')
+BLUE=$(printf '\033[34m')
+PURPLE=$(printf '\033[35m')
+RESET=$(printf '\033[m')
+RB_YELLOW=$(printf '\033[93m')
+
 # Prep 1 - Are we "root"
 if [ $UID -eq 0 ]; then
 	sudo=""
@@ -9,26 +18,27 @@ else
 	sudo="sudo"
 fi
 
-echo -e "Welcome to use qemuguide software!\nPlease read README file at first"
-read -r -p "Do you want to continue installation? [Y/n] " input
-
-case $input in
-	[yY][eE][sS]|[yY]|"")
+printf "%s\n" "${GREEN}Welcome to use qemuguide software!${RESET}"
+printf "%s\n" "${RB_YELLOW}Please read ${YELLOW}README ${RB_YELLOW}file at first${RESET}"
+printf "%s\n" "${RB_YELLOW}Do you want to ${BLUE}continue ${RB_YELLOW}installation? ${PURPLE}[Y/n]${RESET}"
+printf "%s\n" "Press ${GREEN}enter${RESET} to ${BLUE}continue${RESET}, type ${YELLOW}n${RESET} to ${BLUE}exit.${RESET}"
+read -r opt
+case $opt in
+	y* | Y* | "")
 		$sudo cp qemuguide qemuguidecreate qemubootexisting qemubootdisk qemudelete /usr/bin
-		if [[ ! -d "$HOME/.themes" ]]; then
-                mkdir -p "$HOME/.themes"
+		if [[ ! -d "${HOME}/qemu_vms" ]]; then
+			mkdir -p "${HOME}/qemu_vms"
 		fi
-		echo "Installation Completed"
+		printf "%s\n" "${GREEN}Your virtual machine files path is the${RESET} ${YELLOW}\"${HOME}/qemu_vms\"${RESET} ${GREEN}folder${RESET}"
+		printf "%s\n" "${GREEN}Installation Completed!${RESET}"
 		exit 0
 		;;
-
-	[nN][oO]|[nN])
-		echo "installation refused by $USER: `date`"
+	n* | N*)
+		printf "%s\n" "${RED}installation refused by $USER: `date`${RESET}"
 		exit 1
 		;;
-
 	*)
-		echo "Invalid input..."
+		printf "%s\n" "${RED}Invalid choice. exited.${RESET}"
 		exit 1
 		;;
 esac
--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,15 @@
 set -u
 set -e
 
+# define some colors for output
+RED=$(printf '\033[31m')
+GREEN=$(printf '\033[32m')
+YELLOW=$(printf '\033[33m')
+BLUE=$(printf '\033[34m')
+PURPLE=$(printf '\033[35m')
+RESET=$(printf '\033[m')
+RB_YELLOW=$(printf '\033[93m')
+
 # Prep 1 - Are we "root"
 if [ $UID -eq 0 ]; then
 	sudo=""
@@ -9,45 +18,50 @@ else
 	sudo="sudo"
 fi
 
+# check qemu script
 if [[ ! -f "/usr/bin/qemuguide" ]] && \
 	[[ ! -f "/usr/bin/qemuguidecreate" ]] && \
 	[[ ! -f "/usr/bin/qemubootexisting" ]] && \
 	[[ ! -f "/usr/bin/qemubootdisk" ]] && \
 	[[ ! -f "/usr/bin/qemudelete" ]]; then
-	echo "You have not installed the script, please install it first"
+	printf "%s\n" "${RED}You have not installed the script, please install it first${RESET}"
 	exit 1
 fi
 
-echo "You are uninstalling qemuguide from your computer"
-read -r -p "Do you want to continue? [Y/n] " input
+delete_virtual_machines() {
+	printf "%s\n" "${RB_YELLOW}Do you want to ${BLUE}delete all virtual machines? ${PURPLE}[Y/n]${RESET}"
+	printf "%s\n" "Press ${GREEN}enter${RESET} to ${BLUE}continue${RESET}, type ${YELLOW}n${RESET} to ${BLUE}skipped.${RESET}"
+	read -r opt
+	case $opt in
+		y* | Y* | "")
+			$sudo rm -r ${HOME}/qemu_vms
+			printf "%s\n" "${GREEN}All virtual machings have been removed from you computer${RESET}"
+			;;
+		n* | N*)
+			;;
+		*)
+			printf "%s\n" "Invalid choice. skipped."
+			;;
+	esac
+}
 
-case $input in
-	[yY][eE][sS]|[yY]|"")
-	read -r -p "Do you want to delete all virtual machines? [Y/n]" input
-case $input in
-	[yY][eE][sS]|[yY]|"")
-		$sudo rm -r ~/qemu_vms
-		echo "All virtual machings have been removed from you computer"
-		;;
-	[nN][oO]|[nN])
-		;;
-
-	*)
-		echo "Invalid input..."
-		exit 1
-		;;
-esac
+printf "%s\n" "${RB_YELLOW}You are uninstalling qemuguide from your computer${RESET}"
+printf "%s\n" "${RB_YELLOW}Do you want to ${BLUE}continue? ${PURPLE}[Y/n]${RESET}"
+printf "%s\n" "Press ${GREEN}enter${RESET} to ${BLUE}continue${RESET}, type ${YELLOW}n${RESET} to ${BLUE}exit.${RESET}"
+read -r opt
+case $opt in
+	y* | Y* | "")
 		$sudo rm /usr/bin/qemuguide /usr/bin/qemuguidecreate /usr/bin/qemubootexisting /usr/bin/qemubootdisk /usr/bin/qemudelete
-		echo "Uninstallation Completed"
+		delete_virtual_machines
+		printf "%s\n" "${GREEN}Uninstallation Completed!${RESET}"
 		exit 0
 		;;
-	[nN][oO]|[nN])
-		echo "Uninstallation refused by $USER: `date`"
+	n* | N*)
+		printf "%s\n" "${RED}Uninstallation refused by $USER: `date`${RESET}"
 		exit 1
 		;;
-
 	*)
-		echo "Invalid input..."
+		printf "%s\n" "${RED}Invalid choice. exited.${RESET}"
 		exit 1
 		;;
 esac
